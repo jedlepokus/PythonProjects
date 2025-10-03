af7d4f939bfea5e76be62c4025f03e2cf2877568
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:26:15 +0200

    prvni commit

--- main.py
+++ main.py
@@ -1,2 +1,56 @@
 print("Hello world")
-prnit("ahoj")
+
+a = "ahoj"
+b = a
+
+print(b)  # komentář
+# komentar
+# ***********************
+# dulezita cast kodu
+# ****************
+
+# datove typy v pytohnu
+
+samopopisne_nazvy_promennych = "ahoj"
+print(samopopisne_nazvy_promennych)
+print(type(samopopisne_nazvy_promennych))
+
+cislo = 9
+print(cislo)
+print(type(cislo))
+
+desetinne_cislo = 5.5
+print(desetinne_cislo)
+print(type(desetinne_cislo))
+
+vysledek = 5 / 7
+print(vysledek)
+print(type(vysledek))
+
+a = "ahoj"
+a = 5
+
+# cisla
+
+a = 5
+b = 7
+c = a + b
+d = a - b
+e = a * b
+f = a / b # b!=0
+g = a ** 2
+h = a ** (1/2)
+i = a % b
+
+print(c)
+
+x = 5.5
+print(round(x))
+print(int(x))
+
+y = 5 + (5 * 2)
+
+logicka = True
+l2 = False
+
+llll = logicka and l2 or logicka
