74b4b383c55aa90f34f452021c4217307bb26f4f
D32\zak <spseplzen@spseplzen.cz>
2026-02-13 11:30:38 +0100

    uklid

--- zaklady/guess_the_number.py
+++ zaklady/guess_the_number.py
@@ -4,6 +4,7 @@ limit_dolni = 0
 limit_horni = 20
 target = np.random.randint(limit_dolni, limit_horni + 1)
 attempts = 0
+
 while True:
     while True:
         guess_text = input("Hadej cislo od 0 do 20 (vcetne): ")
